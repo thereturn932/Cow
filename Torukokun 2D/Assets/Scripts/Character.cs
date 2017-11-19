579ab8e5a15d1500b98f7522e35e696707c56082
thereturn932 <thereturn932@users.noreply.github.com>
2017-11-20 00:35:30 +0300

    Some addition
    Idle animation and movement scripts are added

---
+++ Torukokun 2D/Assets/Scripts/Character.cs
@@ -0,0 +1,49 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    private Rigidbody2D myRigidBody;
+
+    [SerializeField]
+    private float MovementSpeed = 1;
+
+    private bool facingRight;
+
+	// Use this for initialization
+	void Start () {
+        facingRight = true;
+        myRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        HandleMovement(horizontal);
+
+        Flip(horizontal);
+	}
+
+    private void HandleMovement(float horizontal)
+    {
+        myRigidBody.velocity = new Vector2(horizontal * MovementSpeed, myRigidBody.velocity.y);
+
+    }
+
+    private void Flip(float horizontal)
+    {
+        if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale;
+
+            theScale.x *= -1;
+
+            transform.localScale = theScale;
+        }
+
+    }
+}
