f59386b625755f6feb09bc0a5fe0376c20de4b17
thereturn932 <thereturn932@users.noreply.github.com>
2017-11-20 18:37:25 +0300

    Some new files

--- Torukokun 2D/Assets/Scripts/Character.cs
+++ Torukokun 2D/Assets/Scripts/Character.cs
@@ -11,27 +11,88 @@ public class Character : MonoBehaviour {
     [SerializeField]
     private float MovementSpeed = 1;
 
+    private bool isAttacking;
+
     private bool facingRight;
 
+    [SerializeField]
+    private Transform[] groundPoints;
+
+    [SerializeField]
+    private float groundRadius;
+
+    [SerializeField]
+    private float jumpForce;
+
+    [SerializeField]
+    private LayerMask whatIsGround;
+
+    private bool isGrounded;
+    private bool jump;
+
+    [SerializeField]
+    private bool airControl;
+    
 	// Use this for initialization
 	void Start () {
         facingRight = true;
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+    void Update()
+    {
+        HandleInput();    
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
         float horizontal = Input.GetAxis("Horizontal");
+
+        isGrounded = IsGrounded();
 
         HandleMovement(horizontal);
 
         Flip(horizontal);
+
+        HandleAttacks();
+
+        ResetValues();
 	}
+
+    private void HandleAttacks()
+    {
+        if (isAttacking && !this.myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+        {
+            myAnimator.SetTrigger("attack");
+            myRigidBody.velocity = Vector2.zero;
+        }
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            isAttacking = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump = true;
+        }
+
+    }
 
     private void HandleMovement(float horizontal)
     {
-        myRigidBody.velocity = new Vector2(horizontal * MovementSpeed, myRigidBody.velocity.y);
+        if (!this.myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+        {
+            myRigidBody.velocity = new Vector2(horizontal * MovementSpeed, myRigidBody.velocity.y);
+        }
+        if (isGrounded && jump)
+        {
+            isGrounded = false;
+            myRigidBody.AddForce(new Vector2(0, jumpForce));
+        }
 
         myAnimator.SetFloat("speed", Mathf.Abs(horizontal));
     }
@@ -49,5 +110,32 @@ public class Character : MonoBehaviour {
             transform.localScale = theScale;
         }
 
+    }
+
+    private void ResetValues()
+    {
+        isAttacking = false;
+        jump = false;
+    }
+
+    private bool IsGrounded()
+    {
+        if (myRigidBody.velocity.y <= 0)
+        {
+            foreach (Transform point in groundPoints)
+            {
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(point.position, groundRadius, whatIsGround);
+
+                for (int i = 0; i < colliders.Length; i++)
+                {
+                    if (colliders[i].gameObject != gameObject)
+                    {
+                        return true;
+                    }
+                }
+
+            }    
+        }
+        return false;
     }
 }
